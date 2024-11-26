8495857a2909e7333583022b826982611e6a4335
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-11-26 23:40:06 +0100

    contact form validation, error message

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,18 @@
 import { useId } from "react";
 import css from "./ContactForm.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const ContactSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  number: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
 const initialValues = { name: "", number: "" };
 
@@ -14,16 +26,23 @@ export default function ContactForm({ addContact }) {
   }
 
   return (
-    <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+      validationSchema={ContactSchema}
+    >
       <Form>
         <label htmlFor={nameId}>
           <span>Name</span>
           <Field type="text" name="name" id={nameId} />
+          <ErrorMessage component="p" name="name" />
         </label>
 
         <label htmlFor={numberId}>
           <span>Number</span>
           <Field type="text" name="number" id={numberId} />
+
+          <ErrorMessage className={css.error} component="p" name="number" />
         </label>
 
         <button type="submit">Add contact</button>
